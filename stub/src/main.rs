1f48a402ab8e598d69394e8798bec7dd39e3999a
Jarl Evanson <evanson.jarl@gmail.com>
2025-11-09 19:04:20 -0600

    stub: implement framework for generic panic handling

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -3,6 +3,8 @@
 #![no_std]
 #![no_main]
 
+use sync::Spinlock;
+
 use crate::boot::Context;
 
 mod boot;
@@ -11,8 +13,14 @@ fn stub_main(context: &mut Context) {
     todo!()
 }
 
+static PANIC_FUNC: Spinlock<fn(&core::panic::PanicInfo) -> !> = Spinlock::new(fallback);
+
 /// Generic handler for panics.
 #[panic_handler]
-fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+    PANIC_FUNC.lock()(info)
+}
+
+fn fallback(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
