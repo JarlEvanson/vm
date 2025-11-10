2a67421a1577a0f370fb8ff88f629b9aabd53303
Jarl Evanson <evanson.jarl@gmail.com>
2025-11-10 15:58:54 +0000

    stub: implement Limine entry point
    The architecture of stub forces UEFI and Limine to share the same entry
    point, which is why the assembly stub has been moved.

--- stub/src/boot/mod.rs
+++ stub/src/boot/mod.rs
@@ -3,6 +3,81 @@
 mod context;
 mod relocation;
 
+mod limine;
 mod uefi;
 
 pub use context::{AllocationPolicy, Context, FailedMapping, NotFound, OutOfMemory};
+
+use limine::limine_main;
+use uefi::uefi_main;
+
+#[cfg(target_arch = "aarch64")]
+core::arch::global_asm! {
+    ".global main",
+    "main:",
+
+    "stp x29, x30, [sp, #-16]",
+    "stp x0, x1, [sp, #-32]",
+    "sub sp, sp, #32",
+
+    "bl relocate",
+    "cmp x0, #0",
+
+    "add sp, sp, #32",
+    "ldp x0, x1, [sp, #-32]",
+    "ldp x29, x30, [sp, #-16]",
+
+    "b.ne 5f", // Branch if `relocate` failed.
+
+    "cbz x0, {limine_main}", // If first argument is zero, jump to limine.
+    "b {uefi_main}",         // Otherwise, jump to UEFI
+
+    "5:",
+    "cbz x0, 6f", // If first argument is zero, spin forever (it's limine).
+
+    // Otherwise, return with x0 = 0x8000000000000001 (LOAD_ERROR).
+    "mov x0, #1",
+    "orr x0, x0, #0x8000000000000000",
+    "ret",
+
+    "6:",
+    "b 6b",
+
+    uefi_main = sym uefi_main,
+    limine_main = sym limine_main,
+}
+
+#[cfg(target_arch = "x86_64")]
+core::arch::global_asm! {
+    ".global main",
+    "main:",
+
+    "push rcx",
+    "push rdx",
+
+    "call relocate",
+
+    "pop rdx",
+    "pop rcx",
+
+    "cmp rax, 0",   // Check for succssful `relocate`.
+    "jne 5f",       // Jump if failed.
+
+    "cmp rcx, 0",
+    "je {limine_main}", // If first argument is zero, jump to Limine.
+    "jmp {uefi_main}",  // Otherwise, jump to UEFI
+
+    "5:",
+    "cmp rcx, 0", // If zero, then spin forever (it's linux).
+    "je 6f",
+
+    // Otherwise, return with rax = 0x8000000000000001 (LOAD_ERROR).
+    "mov rax, 0x8000000000000001",
+    "ret",
+
+    "6:",
+    "jmp 6b",
+
+    uefi_main = sym uefi_main,
+    limine_main = sym limine_main,
+}
--- stub/src/boot/uefi.rs
+++ stub/src/boot/uefi.rs
@@ -20,58 +20,13 @@ use crate::{
     stub_main,
 };
 
-#[cfg(target_arch = "aarch64")]
-core::arch::global_asm! {
-    ".global efi_main",
-    "efi_main:",
-
-    "stp x29, x30, [sp, #-16]",
-    "stp x0, x1, [sp, #-32]",
-    "sub sp, sp, #32",
-
-    "bl relocate",
-    "cmp x0, #0",
-
-    "add sp, sp, #32",
-    "ldp x0, x1, [sp, #-32]",
-    "ldp x29, x30, [sp, #-16]",
-
-    // If `relocate()` was a success, jump to `uefi_main()`.
-    "b.eq {uefi_main}",
-    // Otherwise, return with x0 = 0x8000000000000001 (LOAD_ERROR).
-    "mov x0, #1",
-    "orr x0, x0, #0x8000000000000000",
-    "ret",
-
-    uefi_main = sym uefi_main,
-}
-
-#[cfg(target_arch = "x86_64")]
-core::arch::global_asm! {
-    ".global efi_main",
-    "efi_main:",
-
-    "push rcx",
-    "push rdx",
-
-    "call relocate",
-    "cmp rax, 0",
-
-    "pop rdx",
-    "pop rcx",
-
-    // If `relocate()` was a success, jump to `uefi_main()`.
-    "je {uefi_main}",
-    // Otherwise, return with rax = 0x8000000000000001 (LOAD_ERROR).
-    "mov rax, 0x8000000000000001",
-    "ret",
-
-    uefi_main = sym uefi_main,
-}
-
 static SYSTEM_TABLE_PTR: AtomicPtr<SystemTable> = AtomicPtr::new(ptr::null_mut());
 
-extern "efiapi" fn uefi_main(image_handle: Handle, system_table_ptr: *mut SystemTable) -> Status {
+// Entry point for Rust code when booted using the UEFI boot protocol.
+pub extern "efiapi" fn uefi_main(
+    image_handle: Handle,
+    system_table_ptr: *mut SystemTable,
+) -> Status {
     SYSTEM_TABLE_PTR.store(system_table_ptr, Ordering::Relaxed);
     *crate::PANIC_FUNC.lock() = panic_handler;
 
