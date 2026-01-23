83f84ae58a7cb9ccbba4ce9121e2cec02bcb6acb
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-22 22:41:25 -0600

    Add testing scripts

---
+++ test-aarch64.sh
@@ -0,0 +1,43 @@
+set -e
+
+clear
+cargo fmt
+
+mkdir -p run/aarch64/fat/EFI/BOOT
+cp \
+    ~/projects/ovmf-binaries/aarch64/code.fd \
+    ~/projects/ovmf-binaries/aarch64/vars.fd \
+    run/aarch64/
+
+cp ~/projects/limine-binaries/BOOTAA64.EFI run/aarch64/fat/EFI/BOOT/
+cat <<EOF > run/aarch64/fat/limine.conf
+serial: yes
+verbose: yes
+
+/efi
+protocol: efi
+path: boot():/revm.efi
+
+/linux
+protocol: linux
+path: boot():/revm.efi
+
+/limine
+protocol: limine
+path: boot():/revm.efi
+kaslr: yes
+
+EOF
+
+cargo xtask package --arch aarch64 --profile dev --output-path run/aarch64/fat/revm.efi
+
+qemu-system-aarch64 \
+    -machine virt -cpu a64fx -m 512M \
+    -drive if=pflash,format=raw,file=run/aarch64/code.fd \
+    -drive if=pflash,format=raw,file=run/aarch64/vars.fd \
+    -drive file=fat:rw:run/aarch64/fat,format=raw \
+    -device ramfb \
+    -device qemu-xhci -device usb-kbd \
+    -serial file:run/aarch64/serial.txt \
+    -D run/aarch64/qemu-log.txt -d int -d cpu_reset \
+    -s
