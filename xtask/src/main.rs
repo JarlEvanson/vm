589dc2a107a3b8e429527ab9ac65a12e5fb64038
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-22 22:01:35 -0600

    xtask: implement `build-stub`

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,21 @@
 //! Automation for analyzing, building, formatting, packaging, and testing `revm` and associated
 //! executables and other assets.
 
-fn main() {}
+use anyhow::Result;
+
+use crate::{action::build_stub::build_revm_stub, cli::Action};
+
+pub mod action;
+pub mod cli;
+pub mod common;
+
+fn main() -> Result<()> {
+    match cli::get_action() {
+        Action::BuildStub(config) => {
+            let path = build_revm_stub(config)?;
+            println!("revm_stub located at \"{}\"", path.display());
+        }
+    }
+
+    Ok(())
+}
