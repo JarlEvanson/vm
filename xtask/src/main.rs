529cb893d876137c2ba4efdb070bccd619fa1c60
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-22 22:03:17 -0600

    xtask: implement `build-revm`

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,7 +3,10 @@
 
 use anyhow::Result;
 
-use crate::{action::build_stub::build_revm_stub, cli::Action};
+use crate::{
+    action::{build_revm::build_revm, build_stub::build_revm_stub},
+    cli::Action,
+};
 
 pub mod action;
 pub mod cli;
@@ -14,6 +17,10 @@ fn main() -> Result<()> {
         Action::BuildStub(config) => {
             let path = build_revm_stub(config)?;
             println!("revm_stub located at \"{}\"", path.display());
+        }
+        Action::BuildRevm(config) => {
+            let path = build_revm(config)?;
+            println!("revm located at \"{}\"", path.display());
         }
     }
 
