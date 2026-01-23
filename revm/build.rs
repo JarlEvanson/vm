8b7b1f9d67fe28b0965450e38a2291e368f8767f
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-22 21:40:55 -0600

    revm: create basic crate

---
+++ revm/build.rs
@@ -0,0 +1,10 @@
+//! Build script for `revm`.
+
+fn main() -> Result<(), ()> {
+    let manifest_dir =
+        std::env::var("CARGO_MANIFEST_DIR").expect("cargo didn't pass CARGO_MANIFEST_DIR");
+
+    println!("cargo::rustc-link-arg=-T{manifest_dir}/linker-script.ld");
+
+    Ok(())
+}
---
+++ revm/src/main.rs
@@ -0,0 +1,12 @@
+//! # `revm`
+//!
+//! `revm` is a platform for hardware probing, remote debugging, and black-box reverse engineering.
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+
+/// Generic handler for panics.
+#[panic_handler]
+#[cfg(not(test))]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
