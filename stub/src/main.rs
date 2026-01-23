449b68a8723d1227d15ac1ab02eb6845e33dcf25
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-22 21:41:51 -0600

    stub: create basic crate

---
+++ stub/src/main.rs
@@ -0,0 +1,10 @@
+//! The first stage loader for the `revm` platform.
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+
+/// Generic handler for panics.
+#[panic_handler]
+#[cfg(not(test))]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
