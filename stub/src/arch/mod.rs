1d821584eaf7d85ec540ed3892c50dcd9d2c8548
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-25 20:21:46 -0600

    stub: implement 4 and 5 level paging on `x86_64`

--- stub/src/arch/mod.rs
+++ stub/src/arch/mod.rs
@@ -1,3 +1,9 @@
 //! Architecture-specific code.
 
+#[cfg(target_arch = "x86_64")]
+mod x86_64;
+
 pub mod generic;
+
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::address_space::X86_64AddressSpace as AddressSpaceImpl;
