0bb0ca7bf58cd1e30746282937dcc7f7375d67e7
Jarl Evanson <evanson.jarl@gmail.com>
2026-01-29 19:09:31 +0000

    stub: implement `stub_main()`

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -2,11 +2,29 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+use core::{error, fmt};
+
 use sync::Spinlock;
 
 pub mod arch;
 pub mod platform;
 pub mod util;
+
+/// Entry point used after all boot protocol and architecture specific code has been run.
+fn stub_main() -> Result<(), StubError> {
+    Ok(())
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum StubError {}
+
+impl fmt::Display for StubError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {}
+    }
+}
+
+impl error::Error for StubError {}
 
 /// The platform-specific panic handler function.
 static PANIC_FUNC: Spinlock<fn(&core::panic::PanicInfo) -> !> = Spinlock::new(fallback);
