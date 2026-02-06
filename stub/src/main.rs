ba6ff2ef8d7c6f2b53b67831a82ea1c5d0991da4
Jarl Evanson <evanson.jarl@gmail.com>
2026-02-06 21:35:15 +0000

    stub: implement platform-specific panic handling mechanism

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -2,11 +2,23 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+use sync::Spinlock;
+
 pub mod platform;
+
+/// The platform-specific panic handler function.
+static PANIC_FUNC: Spinlock<fn(&core::panic::PanicInfo) -> !> = Spinlock::new(fallback);
 
 /// Generic handler for panics.
 #[panic_handler]
 #[cfg(not(test))]
-fn panic_handler(_: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+    PANIC_FUNC.lock()(info)
+}
+
+/// The panic handler function utilized if no other panic handler is assigned.
+fn fallback(_: &core::panic::PanicInfo) -> ! {
+    loop {
+        core::hint::spin_loop()
+    }
 }
