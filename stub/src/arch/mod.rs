16953ca63371483b1f5a64225037c515f6eb676d
Jarl Evanson <evanson.jarl@gmail.com>
2026-02-07 12:02:14 +0000

    stub: implement `x86_32` and `x86_64` paging implementations

--- stub/src/arch/mod.rs
+++ stub/src/arch/mod.rs
@@ -1,3 +1,6 @@
 //! Architecture-specific code.
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod x86_common;
+
 pub mod generic;
---
+++ stub/src/arch/x86_common/paging/mod.rs
@@ -0,0 +1,5 @@
+//! Paging structures that are common to `x86_32` and `x86_64`.
+
+pub mod bits_32;
+pub mod long_mode;
+pub mod pae;
