bef918fce737d69dbe89900512654c4cc3417026
Jarl Evanson <evanson.jarl@gmail.com>
2026-02-15 04:18:17 +0000

    stub: implement wrapper functions over `Platform` API

---
+++ stub/src/platform/generic/frame_allocation.rs
@@ -0,0 +1,398 @@
+//! Implementation of the frame allocation API provided to the remainder of `revm-stub`.
+
+use core::{mem, ptr::NonNull};
+
+use memory::address::FrameRange;
+use sync::Spinlock;
+
+use crate::platform::{
+    allocate, deallocate,
+    generic::{AllocationPolicy, OutOfMemory, platform},
+};
+
+/// The head of the linked list of [`AllocationRecord`]s.
+static HEAD: Spinlock<Option<SendPtr<AllocationRecord>>> = Spinlock::new(None);
+
+/// Returns the size, in bytes, of a frame.
+pub fn frame_size() -> u64 {
+    platform().frame_size()
+}
+
+/// Allocates a region of `count` frames in accordance with the provided [`AllocationPolicy`].
+///
+/// # Errors
+///
+/// Returns [`OutOfMemory`] if the system cannot allocate the requested frames. This may not
+/// indicate memory exhaustion if [`AllocationPolicy::Any`] is not in use.
+pub fn allocate_frames(
+    count: u64,
+    policy: AllocationPolicy,
+) -> Result<FrameAllocation, OutOfMemory> {
+    let range = platform().allocate_frames(count, policy)?;
+    if insert_range(range).is_err() {
+        // SAFETY:
+        //
+        // The frame range has not been exposed outside of this module and has not been stored.
+        unsafe { platform().deallocate_frames(range) }
+
+        crate::trace!("allocate_frames({count}, {policy:?}) -> OutOfMemory");
+        return Err(OutOfMemory);
+    };
+
+    crate::trace!("allocate_frames({count}, {policy:?}) -> {range:x?}");
+    Ok(FrameAllocation(range))
+}
+
+/// Allocates a range of `count` frames in accordance with the provided [`AllocationPolicy`]. The
+/// starting physical address of the range will be a multiple of `alignment`.
+///
+/// # Errors
+///
+/// Returns [`OutOfMemory`] if the system cannot allocate the requested frames. This may not
+/// indicate memory exhaustion if [`AllocationPolicy::Any`] is not in use.
+pub fn allocate_frames_aligned(
+    count: u64,
+    alignment: u64,
+    policy: AllocationPolicy,
+) -> Result<FrameAllocation, OutOfMemory> {
+    let range = platform().allocate_frames_aligned(count, alignment, policy)?;
+    if insert_range(range).is_err() {
+        // SAFETY:
+        //
+        // The frame range has not been exposed outside of this module and has not been stored.
+        unsafe { platform().deallocate_frames(range) }
+
+        crate::trace!("allocate_frames_aligned({count}, {alignment}, {policy:?}) -> OutOfMemory");
+        return Err(OutOfMemory);
+    };
+
+    crate::trace!("allocate_frames_aligned({count}, {alignment}, {policy:?}) -> {range:x?}");
+    Ok(FrameAllocation(range))
+}
+
+/// Deallocates the provided [`FrameRange`].
+///
+/// # Safety
+///
+/// The [`AddressChunk`][ac]s contained within the provided [`FrameRange`] must have been
+/// allocated by a call to [`allocate_frames()`] or [`allocate_frames_aligned()`] and must not be
+/// utilized after this function.
+///
+/// [ac]: memory::address::AddressChunk
+pub unsafe fn deallocate_frames(range: FrameRange) {
+    crate::trace!("deallocate_frames({range:x?})");
+    // SAFETY:
+    //
+    // The [`FrameRange`] will not be accessed after this.
+    unsafe { remove_range(range) }
+
+    // SAFETY:
+    //
+    // The frame region will not be accessed after this.
+    unsafe { platform().deallocate_frames(range) }
+}
+
+/// Deallocates all outstanding frame allocations.
+///
+/// # Safety
+///
+/// All [`FrameRange`]s allocated via this module must not be accessed after the start of this
+/// function.
+pub unsafe fn deallocate_all_frames() {
+    let mut head = HEAD.lock();
+
+    let mut current = head.as_ref().map(|wrapper| wrapper.0);
+    while let Some(node) = current {
+        // SAFETY:
+        //
+        // All `Some` values point to a valid [`AllocationRecord`] and the list is
+        // protected by the [`HEAD`] lock.
+        let record = unsafe { node.as_ref() };
+
+        // SAFETY:
+        //
+        // This region was allocated by this module and will not be accessed
+        // again after this call.
+        unsafe {
+            platform().deallocate_frames(record.range);
+        }
+
+        let next = record.next;
+
+        // SAFETY:
+        //
+        // This allocation record is no longer referenced.
+        unsafe {
+            deallocate(
+                node.cast::<u8>(),
+                mem::size_of::<AllocationRecord>(),
+                mem::align_of::<AllocationRecord>(),
+            );
+        }
+
+        current = next;
+    }
+
+    // Clear list and release lock
+    *head = None;
+}
+
+/// Removes a region of physical memory from the linked list.
+#[expect(clippy::missing_safety_doc)]
+#[expect(clippy::missing_panics_doc)]
+pub unsafe fn remove_range(range: FrameRange) {
+    let mut head = HEAD.lock();
+
+    let mut current = head.as_ref().map(|wrapper| wrapper.0);
+    let mut previous: Option<NonNull<AllocationRecord>> = None;
+    while let Some(mut node) = current {
+        // SAFETY:
+        //
+        // All `Some` values point to a valid [`AllocationRecord`] and this linked list is
+        // protected by the [`HEAD`] lock.
+        let record = unsafe { node.as_mut() };
+
+        // Skip record if its end is less than the target region's start.
+        if record.range.end_exclusive() < range.start() {
+            previous = current;
+            current = record.next;
+            continue;
+        }
+
+        // Exit the loop if the current record's start is greater than or equal to the target
+        // region's end.
+        if record.range.start() >= range.end_exclusive() {
+            break;
+        }
+
+        // We have that:
+        //
+        // record.start < free_end
+        // record.end >= free_start
+        //
+        // This implies that `range` is fully subsumed inside of `record.range`. If that is not the
+        // case, then the requested [`FrameRange`] is not valid for deallocation.
+        let (lower, overlaps, upper) = range.partition(record.range);
+        if overlaps != range {
+            // Drop `head` to enable deallocation in panic handler.
+            drop(head);
+
+            panic!("invalid frame range deallocated");
+        }
+
+        match (!lower.is_empty(), !upper.is_empty()) {
+            (true, true) => {
+                // Allocated regions remain at the start and end.
+                record.range = lower;
+
+                let Some(allocation_record_allocation) = allocate(
+                    mem::size_of::<AllocationRecord>(),
+                    mem::align_of::<AllocationRecord>(),
+                ) else {
+                    // If the allocation fails, we leak the top region of frames to prevent
+                    // unsafety.
+                    return;
+                };
+                let allocation_record_ptr = allocation_record_allocation
+                    .ptr_nonnull()
+                    .cast::<AllocationRecord>();
+
+                record.next = Some(allocation_record_ptr);
+                // SAFETY:
+                //
+                // This memory region was just allocated and thus this function has exclusive
+                // access to it.
+                unsafe {
+                    allocation_record_ptr.write(AllocationRecord {
+                        range: upper,
+                        next: record.next,
+                    })
+                }
+
+                // Forget this [`FrameAllocation`] to prevent early free.
+                mem::forget(allocation_record_allocation);
+                return;
+            }
+            (true, false) => {
+                // Allocated region remains at the start.
+                record.range = lower;
+                return;
+            }
+            (false, true) => {
+                // Allocated region remains at the end.
+                record.range = upper;
+                return;
+            }
+            (false, false) => {
+                // No allocated regions remain.
+                let next = record.next;
+                match previous {
+                    // SAFETY:
+                    //
+                    // All `Some` values point to a valid [`AllocationRecord`] and this linked list
+                    // is protected by the [`HEAD`] lock.
+                    Some(mut previous) => unsafe { previous.as_mut().next = next },
+                    None => *head = next.map(SendPtr),
+                }
+
+                // SAFETY:
+                //
+                // This node is not in use any longer.
+                unsafe {
+                    deallocate(
+                        node.cast::<u8>(),
+                        mem::size_of::<AllocationRecord>(),
+                        mem::align_of::<AllocationRecord>(),
+                    )
+                }
+                return;
+            }
+        }
+    }
+}
+
+/// Inserts a region of physical memory into the linked list.
+fn insert_range(mut range: FrameRange) -> Result<(), OutOfMemory> {
+    let mut head = HEAD.lock();
+
+    // Ordering prevents having to handle deallocating `node` if `current_end` calculation fails.
+    let allocation_record_allocation = allocate(
+        mem::size_of::<AllocationRecord>(),
+        mem::align_of::<AllocationRecord>(),
+    )
+    .ok_or(OutOfMemory)?;
+    let allocation_record_ptr = allocation_record_allocation
+        .ptr_nonnull()
+        .cast::<AllocationRecord>();
+
+    let mut current = head.as_ref().map(|wrapper| wrapper.0);
+    let mut previous: Option<NonNull<AllocationRecord>> = None;
+    while let Some(node) = current {
+        // SAFETY:
+        //
+        // All `Some` values point to a valid [`AllocationRecord`] and this linked list is
+        // protected by the [`HEAD`] lock.
+        let record = unsafe { node.as_ref() };
+
+        if record.range.end_exclusive() < range.start() {
+            previous = current;
+            current = record.next;
+            continue;
+        }
+
+        if record.range.start() > range.end_exclusive() {
+            break;
+        }
+
+        range = range
+            .merge(record.range)
+            .expect("frame range checks failed");
+
+        let next = record.next;
+        match previous {
+            // SAFETY:
+            //
+            // All `Some` values point to a valid [`AllocationRecord`] and this linked list is
+            // protected by the [`HEAD`] lock.
+            Some(mut previous) => unsafe { previous.as_mut().next = next },
+            None => *head = next.map(SendPtr),
+        }
+
+        // SAFETY:
+        //
+        // We have removed all references to this memory block and it will not be used after this.
+        unsafe {
+            deallocate(
+                node.cast::<u8>(),
+                mem::size_of::<AllocationRecord>(),
+                mem::align_of::<AllocationRecord>(),
+            )
+        }
+
+        current = next;
+    }
+
+    // SAFETY:
+    //
+    // The region of memory pointed to by `allocation_record_ptr` was just allocated and thus is
+    // under the exclusive control of this module.
+    unsafe {
+        allocation_record_ptr.write(AllocationRecord {
+            range,
+            next: current,
+        })
+    }
+
+    match previous {
+        // SAFETY:
+        //
+        // All `Some` values point to a valid [`AllocationRecord`] and this linked list is
+        // protected by the [`HEAD`] lock.
+        Some(mut previous) => unsafe { previous.as_mut().next = Some(allocation_record_ptr) },
+        None => *head = Some(SendPtr(allocation_record_ptr)),
+    }
+
+    // Forget this [`FrameAllocation`] to prevent early free.
+    mem::forget(allocation_record_allocation);
+    Ok(())
+}
+
+/// Wrapper around a region of frames allocated with [`allocate_frames()`] or
+/// [`allocate_frames_aligned()`].
+///
+/// This structure automatically frees the region of frames when dropped.
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct FrameAllocation(FrameRange);
+
+impl FrameAllocation {
+    /// Returns the [`FrameRange`] that this [`FrameAllocation`] owns.
+    pub const fn range(&self) -> FrameRange {
+        self.0
+    }
+}
+
+impl Drop for FrameAllocation {
+    fn drop(&mut self) {
+        // SAFETY:
+        //
+        // The region of frames indicated by `self.physical_address` and `self.count` is under the
+        // exclusive control of [`deallocate_frames()`].
+        unsafe { deallocate_frames(self.0) }
+    }
+}
+
+/// A record of an allocated [`FrameRange`].
+///
+/// This may have been merged with adjacent [`FrameRange`]s.
+struct AllocationRecord {
+    /// The
+    range: FrameRange,
+    /// The next [`AllocationRecord`] in the linked list.
+    next: Option<NonNull<AllocationRecord>>,
+}
+
+// SAFETY:
+//
+// [`AllocationRecord`]s are safe to send to other threads, since they don't expose anything
+// that requires a single thread.
+unsafe impl Send for AllocationRecord {}
+
+// SAFETY:
+//
+// [`AllocationRecord`]s are safe to send to other threads, since they don't expose anything
+// that requires a single thread.
+unsafe impl Sync for AllocationRecord {}
+/// Sync wrapper.
+#[repr(transparent)]
+struct SendPtr<T>(NonNull<T>);
+
+// SAFETY:
+//
+// [`SendPtr<T>`]s are safe to send to other threads, since they don't expose anything
+// that requires a single thread.
+unsafe impl<T> Send for SendPtr<T> {}
+// SAFETY:
+//
+// [`SendPtr<T>`]s are safe to read from other threads, since they don't expose anything that
+// requires a single thread.
+unsafe impl<T> Sync for SendPtr<T> {}
--- stub/src/platform/generic/mod.rs
+++ stub/src/platform/generic/mod.rs
@@ -1,11 +1,27 @@
 //! Definitions providing the interface platform implementors provide and an abstraction utilizing
 //! that interface to implement services for use by the rest of the executable.
 
-use core::{error, fmt, ptr::NonNull};
+use core::{
+    error, fmt,
+    ptr::{self, NonNull},
+    slice,
+};
 
-use memory::address::{FrameRange, PhysicalAddress, PhysicalAddressRange, VirtualAddress};
+use conversion::{u64_to_usize_strict, usize_to_u64};
+use memory::{
+    address::{
+        AddressSpaceDescriptor, FrameRange, PhysicalAddress, PhysicalAddressRange, VirtualAddress,
+    },
+    phys::PhysicalMemorySpace,
+};
 use stub_api::{MemoryDescriptor, Status, TakeoverFlags};
 use sync::ControlledModificationCell;
+
+mod allocation;
+mod frame_allocation;
+
+pub use allocation::*;
+pub use frame_allocation::*;
 
 /// The [`Platform`] implementation that is currently in use.
 static PLATFORM: ControlledModificationCell<Option<&'static dyn Platform>> =
@@ -42,6 +58,327 @@ pub(in crate::platform) fn platform() -> &'static dyn Platform {
     PLATFORM
         .get()
         .expect("platform implementation not initialized")
+}
+
+/// Returns the current physical [`MemoryMap`].
+///
+/// # Errors
+///
+/// [`BufferTooSmall`] is returned when the provided `buffer` is too small, and contains the
+/// required size of the buffer. Any allocations or deallocations may change the memory map.
+pub fn memory_map<'buffer>(
+    buffer: &'buffer mut [MemoryDescriptor],
+) -> Result<MemoryMap<'buffer>, BufferTooSmall> {
+    platform().memory_map(buffer)
+}
+
+/// Returns the size, in bytes, of a [`Page`][memory::address::Page].
+pub fn page_size() -> usize {
+    platform().page_size()
+}
+
+/// Maps [`Platform::page_size()`]ed physical memory region inside which `physical_address` is
+/// contained into the stub's virtual address space. Any call to this function will invalidate
+/// all previous temporary mappings produced by [`Platform::map_temporary()`].
+///
+/// This means that if `physical_address` is 1 byte from the top of a [`Platform::page_size()`]
+/// chunk, only 1 byte may be accessible.
+fn map_temporary(address: PhysicalAddress) -> *mut u8 {
+    platform().map_temporary(address)
+}
+
+/// Implementation of [`PhysicalMemorySpace`] utilizing [`map_temporary()`].
+pub struct StubPhysicalMemory;
+
+// SAFETY:
+//
+// The provided primitives are combined correctly.
+unsafe impl PhysicalMemorySpace for StubPhysicalMemory {
+    type Error = core::convert::Infallible;
+
+    fn descriptor(&self) -> AddressSpaceDescriptor {
+        memory::address::AddressSpaceDescriptor::new(64, false)
+    }
+
+    unsafe fn read_u8(&self, address: PhysicalAddress) -> Result<u8, Self::Error> {
+        let mut byte = 0;
+        read_bytes_at(address, slice::from_mut(&mut byte));
+
+        Ok(byte)
+    }
+
+    unsafe fn read_u16_le(&self, address: PhysicalAddress) -> Result<u16, Self::Error> {
+        let mut bytes = [0; 2];
+        read_bytes_at(address, &mut bytes);
+
+        Ok(u16::from_le_bytes(bytes))
+    }
+
+    unsafe fn read_u16_be(&self, address: PhysicalAddress) -> Result<u16, Self::Error> {
+        let mut bytes = [0; 2];
+        read_bytes_at(address, &mut bytes);
+
+        Ok(u16::from_be_bytes(bytes))
+    }
+
+    unsafe fn read_u32_le(&self, address: PhysicalAddress) -> Result<u32, Self::Error> {
+        let mut bytes = [0; 4];
+        read_bytes_at(address, &mut bytes);
+
+        Ok(u32::from_le_bytes(bytes))
+    }
+
+    unsafe fn read_u32_be(&self, address: PhysicalAddress) -> Result<u32, Self::Error> {
+        let mut bytes = [0; 4];
+        read_bytes_at(address, &mut bytes);
+
+        Ok(u32::from_be_bytes(bytes))
+    }
+
+    unsafe fn read_u64_le(&self, address: PhysicalAddress) -> Result<u64, Self::Error> {
+        let mut bytes = [0; 8];
+        read_bytes_at(address, &mut bytes);
+
+        Ok(u64::from_le_bytes(bytes))
+    }
+
+    unsafe fn read_u64_be(&self, address: PhysicalAddress) -> Result<u64, Self::Error> {
+        let mut bytes = [0; 8];
+        read_bytes_at(address, &mut bytes);
+
+        Ok(u64::from_be_bytes(bytes))
+    }
+
+    unsafe fn write_u8(&mut self, address: PhysicalAddress, value: u8) -> Result<(), Self::Error> {
+        write_bytes_at(address, slice::from_ref(&value));
+        Ok(())
+    }
+
+    unsafe fn write_u16_le(
+        &mut self,
+        address: PhysicalAddress,
+        value: u16,
+    ) -> Result<(), Self::Error> {
+        write_bytes_at(address, &value.to_le_bytes());
+        Ok(())
+    }
+
+    unsafe fn write_u16_be(
+        &mut self,
+        address: PhysicalAddress,
+        value: u16,
+    ) -> Result<(), Self::Error> {
+        write_bytes_at(address, &value.to_be_bytes());
+        Ok(())
+    }
+
+    unsafe fn write_u32_le(
+        &mut self,
+        address: PhysicalAddress,
+        value: u32,
+    ) -> Result<(), Self::Error> {
+        write_bytes_at(address, &value.to_le_bytes());
+        Ok(())
+    }
+
+    unsafe fn write_u32_be(
+        &mut self,
+        address: PhysicalAddress,
+        value: u32,
+    ) -> Result<(), Self::Error> {
+        write_bytes_at(address, &value.to_be_bytes());
+        Ok(())
+    }
+
+    unsafe fn write_u64_le(
+        &mut self,
+        address: PhysicalAddress,
+        value: u64,
+    ) -> Result<(), Self::Error> {
+        write_bytes_at(address, &value.to_le_bytes());
+        Ok(())
+    }
+
+    unsafe fn write_u64_be(
+        &mut self,
+        address: PhysicalAddress,
+        value: u64,
+    ) -> Result<(), Self::Error> {
+        write_bytes_at(address, &value.to_be_bytes());
+        Ok(())
+    }
+}
+
+/// Reads the bytes located at `address` into the provided `bytes` slice.
+///
+/// This function calls [`map_temporary()`].
+pub fn read_bytes_at(mut address: PhysicalAddress, mut bytes: &mut [u8]) {
+    let page_size = usize_to_u64(page_size());
+    while !bytes.is_empty() {
+        // Calculate the number of bytes we will read this iteration.
+        let size = u64_to_usize_strict(
+            (page_size - address.value() % page_size).min(usize_to_u64(bytes.len())),
+        );
+
+        // Map the page we will modify.
+        let ptr = map_temporary(address);
+
+        // SAFETY:
+        //
+        // The region that starts at `ptr` was a valid mapping and `size` represents the
+        // minimum of the mapping size or the size of the remaining bytes to be copied.
+        let read_slice = unsafe { slice::from_raw_parts(ptr, size) };
+        bytes[..size].copy_from_slice(read_slice);
+
+        address = PhysicalAddress::new(address.value().wrapping_add(usize_to_u64(size)));
+        bytes = &mut bytes[size..];
+    }
+}
+
+/// Writes the bytes in `bytes` into the physical memory located at `address`.
+///
+/// This function calls [`map_temporary()`].
+pub fn write_bytes_at(mut address: PhysicalAddress, mut bytes: &[u8]) {
+    let page_size = usize_to_u64(page_size());
+    while !bytes.is_empty() {
+        // Calculate the number of bytes we will write this iteration.
+        let size = u64_to_usize_strict(
+            (page_size - address.value() % page_size).min(usize_to_u64(bytes.len())),
+        );
+        // Map the page we will modify.
+        let ptr = map_temporary(address);
+
+        // SAFETY:
+        //
+        // The region that starts at `ptr` was a valid mapping and `size` represents the
+        // minimum of the mapping size or the size of the bytes that have not yet been written.
+        unsafe { ptr::copy(bytes.as_ptr(), ptr, size) }
+
+        address = PhysicalAddress::new(address.value().wrapping_add(usize_to_u64(size)));
+        bytes = &bytes[size..];
+    }
+}
+
+/// Maps the physical memory region of `size` bytes into `revm-stub`'s virtual address space.
+///
+/// # Panics
+///
+/// This function may panic if the provided implementation produces an incorrect response.
+pub fn map_identity(range: PhysicalAddressRange) -> *mut u8 {
+    let ptr = platform().map_identity(range);
+    assert_eq!(
+        usize_to_u64(ptr.addr()),
+        range.start().value(),
+        "identity map implementation failed"
+    );
+    ptr
+}
+/// Returns the [`PhysicalAddress`] associated with the provided `virtual_address` if the
+/// translation is valid. Otherwise, return [`None`].
+pub fn translate_virtual(address: VirtualAddress) -> Option<PhysicalAddress> {
+    platform().translate_virtual(address)
+}
+
+/// Executes a takover on behalf of the loaded executable. `key` is utilized to ensure that the
+/// memory map that the loaded executable has is accurate.
+///
+/// On success, the executable becomes the sole controller of the system. This means that the
+/// executable is free to directly manipulate the hardware in whatever manner it desires.
+pub fn takeover(key: u64, flags: TakeoverFlags) -> stub_api::Status {
+    platform().takeover(key, flags)
+}
+
+/// Returns the [`PhysicalAddress`] of the UEFI system table, if present.
+pub fn uefi_system_table() -> Option<PhysicalAddress> {
+    platform().uefi_system_table()
+}
+
+/// Returns the [`PhysicalAddress`] of the RSDP structure, if present.
+pub fn rsdp() -> Option<PhysicalAddress> {
+    platform().rsdp()
+}
+
+/// Returns the [`PhysicalAddress`] of the XSDP structure, if present.
+pub fn xsdp() -> Option<PhysicalAddress> {
+    platform().xsdp()
+}
+
+/// Returns the [`PhysicalAddress`] of the device tree structure, if present.
+pub fn device_tree() -> Option<PhysicalAddress> {
+    platform().device_tree()
+}
+
+/// Returns the [`PhysicalAddress`] of the SMBIO 32 Entry Point, if present.
+pub fn smbios_32() -> Option<PhysicalAddress> {
+    platform().smbios_32()
+}
+
+/// Returns the [`PhysicalAddress`] of the SMBIO 64 Entry Point, if present.
+pub fn smbios_64() -> Option<PhysicalAddress> {
+    platform().smbios_64()
+}
+
+/// Prints the provided [`fmt::Arguments`] to the platform's output device.
+pub fn print(args: fmt::Arguments) {
+    platform().print(args)
+}
+
+#[doc(hidden)]
+pub fn _log(level: LogLevel, args: fmt::Arguments) {
+    if level >= LogLevel::Trace {
+        match level {
+            LogLevel::Trace => platform().print(format_args!("TRACE: {args}\n")),
+            LogLevel::Debug => platform().print(format_args!("DEBUG: {args}\n")),
+            LogLevel::Info => platform().print(format_args!("INFO : {args}\n")),
+            LogLevel::Warn => platform().print(format_args!("WARN : {args}\n")),
+            LogLevel::Error => platform().print(format_args!("ERROR: {args}\n")),
+        }
+    }
+}
+
+/// Logs a message with [`LogLevel::Trace`].
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => ($crate::platform::_log(
+        $crate::platform::LogLevel::Trace,
+        format_args!($($arg)*))
+    );
+}
+
+/// Logs a message with [`LogLevel::Debug`].
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => ($crate::platform::_log(
+        $crate::platform::LogLevel::Debug,
+        format_args!($($arg)*))
+    );
+}
+
+/// Logs a message with [`LogLevel::Info`].
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => ($crate::platform::_log(
+        $crate::platform::LogLevel::Info,
+        format_args!($($arg)*))
+    );
+}
+
+/// Logs a message with [`LogLevel::Warn`].
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => ($crate::platform::_log(
+        $crate::platform::LogLevel::Warn,
+        format_args!($($arg)*))
+    );
+}
+
+/// Logs a message with [`LogLevel::Error`].
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => ($crate::platform::_log(
+        $crate::platform::LogLevel::Error,
+        format_args!($($arg)*))
+    );
 }
 
 /// Collection of various services provided by a platform-specific implementation.
@@ -265,3 +602,18 @@ impl fmt::Display for BufferTooSmall {
 }
 
 impl error::Error for BufferTooSmall {}
+
+/// Various levels to determine the priority of information.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LogLevel {
+    /// Designates very low priority information.
+    Trace,
+    /// Designates lower priority information.
+    Debug,
+    /// Designates informatory logs.
+    Info,
+    /// Designates hazardous logs.
+    Warn,
+    /// Designates very serious logs.
+    Error,
+}
