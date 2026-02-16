29bf9a53b4435924060b8d61ce662153642a0218
Jarl Evanson <evanson.jarl@gmail.com>
2026-02-16 08:38:04 -0600

    revm: implement `find_free_region` for `x86_64`

--- revm/src/arch/mod.rs
+++ revm/src/arch/mod.rs
@@ -14,5 +14,8 @@ mod x86_common;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::compute_page_size;
 
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::paging::find_free_region;
+
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub use x86_common::compute_page_size;
