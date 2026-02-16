40c2741aec9b193bb2b09a2287ed838aa65eb723
Jarl Evanson <evanson.jarl@gmail.com>
2026-02-16 08:35:55 -0600

    revm: implement `BareMetalMemory` for raw physical memory access

--- revm/src/memory/phys/mod.rs
+++ revm/src/memory/phys/mod.rs
@@ -1,13 +1,15 @@
 //! Definitions and implementations of physical memory management APIs for `revm`.
 
-use core::{error, fmt};
+use core::{convert::Infallible, error, fmt, ptr};
 
 use crate::{
+    arch::generic::memory::phys::PhysicalMemory,
     memory::{
         page_frame_size,
         phys::structs::{Frame, FrameRange, PhysicalAddress},
+        virt::map_temporary,
     },
-    util::usize_to_u64,
+    util::{u64_to_usize_panicking, usize_to_u64},
 };
 
 pub mod structs;
@@ -87,3 +89,127 @@ impl fmt::Display for FrameAllocationError {
 }
 
 impl error::Error for FrameAllocationError {}
+
+/// [`PhysicalMemory`] implementation that writes directly to `revm`'s view of physical memory.
+pub struct BareMetalMemory;
+
+impl BareMetalMemory {
+    /// Helper to perform a volatile read of type T at a physical address
+    #[expect(clippy::undocumented_unsafe_blocks)]
+    unsafe fn read_volatile<T>(&self, address: PhysicalAddress) -> T {
+        let offset = address.frame_offset();
+        let page = map_temporary(Frame::containing_address(address));
+        let ptr = ptr::with_exposed_provenance::<T>(
+            page.start_address().value() + u64_to_usize_panicking(offset),
+        );
+
+        unsafe { ptr.read_volatile() }
+    }
+
+    /// Helper to perform a volatile write of type T at a physical address
+    #[expect(clippy::undocumented_unsafe_blocks)]
+    unsafe fn write_volatile<T>(&mut self, address: PhysicalAddress, value: T) {
+        let offset = address.frame_offset();
+        let page = map_temporary(Frame::containing_address(address));
+        let ptr = ptr::with_exposed_provenance_mut::<T>(
+            page.start_address().value() + u64_to_usize_panicking(offset),
+        );
+
+        unsafe { ptr.write_volatile(value) }
+    }
+}
+
+// SAFETY:
+//
+// Virtual memory is utilized to ensure that all physical memory can be accessed.
+#[expect(clippy::undocumented_unsafe_blocks)]
+unsafe impl PhysicalMemory for BareMetalMemory {
+    type Error = Infallible;
+
+    unsafe fn read_u8(&self, address: PhysicalAddress) -> Result<u8, Self::Error> {
+        Ok(unsafe { self.read_volatile(address) })
+    }
+
+    unsafe fn read_u16_le(&self, address: PhysicalAddress) -> Result<u16, Self::Error> {
+        Ok(u16::from_le(unsafe { self.read_volatile(address) }))
+    }
+
+    unsafe fn read_u16_be(&self, address: PhysicalAddress) -> Result<u16, Self::Error> {
+        Ok(u16::from_be(unsafe { self.read_volatile(address) }))
+    }
+
+    unsafe fn read_u32_le(&self, address: PhysicalAddress) -> Result<u32, Self::Error> {
+        Ok(u32::from_le(unsafe { self.read_volatile(address) }))
+    }
+
+    unsafe fn read_u32_be(&self, address: PhysicalAddress) -> Result<u32, Self::Error> {
+        Ok(u32::from_be(unsafe { self.read_volatile(address) }))
+    }
+
+    unsafe fn read_u64_le(&self, address: PhysicalAddress) -> Result<u64, Self::Error> {
+        Ok(u64::from_le(unsafe { self.read_volatile(address) }))
+    }
+
+    unsafe fn read_u64_be(&self, address: PhysicalAddress) -> Result<u64, Self::Error> {
+        Ok(u64::from_be(unsafe { self.read_volatile(address) }))
+    }
+
+    unsafe fn write_u8(&mut self, address: PhysicalAddress, value: u8) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value) };
+        Ok(())
+    }
+
+    unsafe fn write_u16_le(
+        &mut self,
+        address: PhysicalAddress,
+        value: u16,
+    ) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value.to_le()) };
+        Ok(())
+    }
+
+    unsafe fn write_u16_be(
+        &mut self,
+        address: PhysicalAddress,
+        value: u16,
+    ) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value.to_be()) };
+        Ok(())
+    }
+
+    unsafe fn write_u32_le(
+        &mut self,
+        address: PhysicalAddress,
+        value: u32,
+    ) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value.to_le()) };
+        Ok(())
+    }
+
+    unsafe fn write_u32_be(
+        &mut self,
+        address: PhysicalAddress,
+        value: u32,
+    ) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value.to_be()) };
+        Ok(())
+    }
+
+    unsafe fn write_u64_le(
+        &mut self,
+        address: PhysicalAddress,
+        value: u64,
+    ) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value.to_le()) };
+        Ok(())
+    }
+
+    unsafe fn write_u64_be(
+        &mut self,
+        address: PhysicalAddress,
+        value: u64,
+    ) -> Result<(), Self::Error> {
+        unsafe { self.write_volatile(address, value.to_be()) };
+        Ok(())
+    }
+}
