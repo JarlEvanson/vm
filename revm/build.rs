629f99b3c9f69a0f97f825333057b1bbcb7940d6
Jarl Evanson <evanson.jarl@gmail.com>
2026-04-28 08:28:09 +0000

    revm: create basic crate

---
+++ revm/build.rs
@@ -0,0 +1,19 @@
+//! Build script for `revm`.
+
+use std::env;
+
+fn main() -> Result<(), ()> {
+    if let Ok(val) = env::var("CARGO_CFG_PANIC")
+        && val == "unwind"
+    {
+        // `revm` is never built with unwinding panics except for when testing.
+        return Ok(());
+    }
+
+    let manifest_dir =
+        std::env::var("CARGO_MANIFEST_DIR").expect("cargo didn't pass CARGO_MANIFEST_DIR");
+
+    println!("cargo::rustc-link-arg=-T{manifest_dir}/linker-script.ld");
+
+    Ok(())
+}
