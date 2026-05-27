c23fe8ba7aa94b938ec823b063db4ea59ce5a59e
Jarl Evanson <evanson.jarl@gmail.com>
2026-05-27 16:58:08 -0400

    stub: implement UEFI platform abstraction layer

--- stub/src/platform/mod.rs
+++ stub/src/platform/mod.rs
@@ -1,6 +1,11 @@
 //! A collection of supported platforms and various utilities provided by said platforms that are
 //! required to carry out `revm-stub`'s goal.
 
+// Platform support modules.
+mod uefi;
+
+use uefi::uefi_main;
+
 // Other support modules.
 
 mod frame_allocator;
@@ -10,3 +15,80 @@ mod heap_allocator;
 mod relocate;
 
 pub use generic::*;
+
+#[cfg(target_arch = "aarch64")]
+core::arch::global_asm! {
+    ".global main",
+    "main:",
+
+    "stp x29, x30, [sp, #-16]",
+    "stp x0, x1, [sp, #-32]",
+    "sub sp, sp, #32",
+
+    "bl relocate",
+    "cmp x0, #0",
+
+    "add sp, sp, #32",
+    "ldp x0, x1, [sp, #-32]",
+    "ldp x29, x30, [sp, #-16]",
+
+    "b.ne 5f", // Branch if `relocate` failed.
+
+    "b {uefi_main}",
+
+    "5:",
+    // Return with x0 = 0x8000000000000001 (LOAD_ERROR).
+    "mov x0, #1",
+    "orr x0, x0, #0x8000000000000000",
+    "ret",
+
+    uefi_main = sym uefi_main,
+}
+
+#[cfg(target_arch = "x86")]
+core::arch::global_asm! {
+    ".global main",
+    "main:",
+
+    "pusha",
+    "call relocate",
+
+    "cmp eax, 0", // Check for successful `relocate`.
+    "popa",
+
+    "jne 5f",     // Jump if failed.
+    "jmp {uefi_main}",
+
+    "5:",
+    // Return with eax = 0x80000001 (LOAD_ERROR).
+    "mov eax, 0x80000001",
+    "ret",
+
+    uefi_main = sym uefi_main,
+}
+
+#[cfg(target_arch = "x86_64")]
+core::arch::global_asm! {
+    ".global main",
+    "main:",
+
+    "push rcx",
+    "push rdx",
+
+    "call relocate",
+
+    "pop rdx",
+    "pop rcx",
+
+    "cmp rax, 0",   // Check for successful `relocate`.
+    "jne 5f",       // Jump if failed.
+
+    "jmp {uefi_main}",
+
+    "5:",
+    // Return with rax = 0x8000000000000001 (LOAD_ERROR).
+    "mov rax, 0x8000000000000001",
+    "ret",
+
+    uefi_main = sym uefi_main,
+}
