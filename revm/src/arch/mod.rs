8a5ec18bed507eb7ff6f5e26cbe52e2b3de29140
Jarl Evanson <evanson.jarl@gmail.com>
2026-06-01 05:40:13 +0000

    revm: begin architecture-dependent module

---
+++ revm/src/arch/mod.rs
@@ -0,0 +1,17 @@
+//! Architecture-specific functionality.
+
+#[cfg(target_arch = "aarch64")]
+mod aarch64;
+#[cfg(target_arch = "x86")]
+mod i686;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod x86;
+#[cfg(target_arch = "x86_64")]
+mod x86_64;
+
+#[cfg(target_arch = "aarch64")]
+use aarch64 as arch_impl;
+#[cfg(target_arch = "x86")]
+use i686 as arch_impl;
+#[cfg(target_arch = "x86_64")]
+use x86_64 as arch_impl;
---
+++ revm/src/arch/x86/mod.rs
@@ -0,0 +1,1 @@
+//! Functionality shared between `i686` and `x86_64`.
