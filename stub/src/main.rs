3e54c3dbe7afafa5367c664c641022e942e0b21e
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-07 22:58:44 -0400

    stub: initialize subproject

---
+++ stub/src/main.rs
@@ -0,0 +1,9 @@
+//! The loader for the `revm` platform.
+#![no_std]
+#![no_main]
+
+/// Generic handler for panics.
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
