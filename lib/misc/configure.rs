d3bef6aeb3cc3fb58c0d82d9a4d1dedbea06af8c
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 21:14:33 -0500

    sync: implement synchronization primitives

--- lib/misc/configure.rs
+++ lib/misc/configure.rs
@@ -2,7 +2,10 @@ use crate::config::{Config, Subproject};
 
 #[path = "conversion/configure.rs"]
 mod conversion;
+#[path = "sync/configure.rs"]
+mod sync;
 
 pub fn configure(config: &mut Config, subprojects: &mut Vec<Subproject>) {
     conversion::configure(config, subprojects);
+    sync::configure(config, subprojects);
 }
