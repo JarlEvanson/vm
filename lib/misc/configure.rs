3f525a25d78f2db3a60d02eb14ce0a65c2290f37
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 21:16:20 -0500

    memory: implement addressing abstractions

--- lib/misc/configure.rs
+++ lib/misc/configure.rs
@@ -2,10 +2,13 @@ use crate::config::{Config, Subproject};
 
 #[path = "conversion/configure.rs"]
 mod conversion;
+#[path = "memory/configure.rs"]
+mod memory;
 #[path = "sync/configure.rs"]
 mod sync;
 
 pub fn configure(config: &mut Config, subprojects: &mut Vec<Subproject>) {
     conversion::configure(config, subprojects);
+    memory::configure(config, subprojects);
     sync::configure(config, subprojects);
 }
