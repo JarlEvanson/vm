cfc847f8d44ec777ddddcf90a7d5d9a81188a94a
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 20:16:26 -0500

    package: implement tool for package `revm-stub` and `revm`

---
+++ tools/package/configure.rs
@@ -0,0 +1,19 @@
+use crate::config::{Config, Subproject};
+
+pub fn configure(config: &mut Config, subprojects: &mut Vec<Subproject>) {
+    let mut root_module = config.arguments.source_dir.join("tools");
+    root_module.push("package");
+    root_module.push("src");
+    root_module.push("main.rs");
+
+    let mut subproject = Subproject::new("package", root_module);
+    subproject.set_binary(true);
+    subproject.disable_revm();
+    subproject.disable_revm_stub();
+
+    subproject.add_libraries("conversion");
+    subproject.add_libraries("elf");
+    subproject.add_libraries("pe");
+
+    subprojects.push(subproject);
+}
