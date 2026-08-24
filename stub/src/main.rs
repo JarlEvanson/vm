d6d680bf2a1efcf4b6dcb4d2937c04591c163108
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 21:33:49 -0500

    stub: implement platform panic handler interface

--- stub/src/main.rs
+++ stub/src/main.rs
@@ -2,10 +2,26 @@
 #![no_std]
 #![no_main]
 
+use sync::Spinlock;
+
 pub mod platform;
+
+/// The platform-specific panic handler function.
+static PANIC_HANDLER: Spinlock<fn(&core::panic::PanicInfo) -> !> = Spinlock::new(fallback);
 
 /// Generic handler for panics.
 #[panic_handler]
-fn panic_handler(_: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+    let lock = PANIC_HANDLER.lock();
+    let handler = *lock;
+    drop(lock);
+
+    handler(info)
+}
+
+/// The panic handler function utilized if no other panic handler is assigned.
+fn fallback(_: &core::panic::PanicInfo) -> ! {
+    loop {
+        core::hint::spin_loop()
+    }
 }
