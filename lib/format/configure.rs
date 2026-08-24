45b293bf5fb5b741dd8598ddb8593b1df6e75e40
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 20:12:53 -0500

    pe: implement raw definitions

--- lib/format/configure.rs
+++ lib/format/configure.rs
@@ -2,7 +2,10 @@ use crate::config::{Config, Subproject};
 
 #[path = "elf/configure.rs"]
 mod elf;
+#[path = "pe/configure.rs"]
+mod pe;
 
 pub fn configure(config: &mut Config, subprojects: &mut Vec<Subproject>) {
     elf::configure(config, subprojects);
+    pe::configure(config, subprojects);
 }
