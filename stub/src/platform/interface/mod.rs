516e737fbce2f45fa036450b259c3a7842a0f231
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 21:33:34 -0500

    stub: implement platform logging interface

--- stub/src/platform/interface/mod.rs
+++ stub/src/platform/interface/mod.rs
@@ -1,10 +1,12 @@
 //! Definitions and interfaces that platforms utilize to provide services for use by the rest of
 //! the executable.
 
+mod logging;
 mod memory;
 mod processor;
 mod tables;
 
+pub use logging::*;
 pub use memory::*;
 pub use processor::*;
 pub use tables::*;
