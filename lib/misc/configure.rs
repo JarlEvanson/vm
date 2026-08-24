681860df35058b058c84d4226ab0715a9ba11f3e
Jarl Evanson <evanson.jarl@gmail.com>
2026-08-23 21:56:56 -0500

    font: define generic binary font format

--- lib/misc/configure.rs
+++ lib/misc/configure.rs
@@ -2,6 +2,8 @@ use crate::config::{Config, Subproject};
 
 #[path = "conversion/configure.rs"]
 mod conversion;
+#[path = "font/configure.rs"]
+mod font;
 #[path = "memory/configure.rs"]
 mod memory;
 #[path = "sync/configure.rs"]
@@ -11,4 +13,5 @@ pub fn configure(config: &mut Config, subprojects: &mut Vec<Subproject>) {
     conversion::configure(config, subprojects);
     memory::configure(config, subprojects);
     sync::configure(config, subprojects);
+    font::configure(config, subprojects);
 }
---
+++ lib/misc/font/src/lib.rs
@@ -0,0 +1,7 @@
+//! Defines the implementation and interfaces of the `revm-stub` font structures.
+//!
+//! Includes both read-only and writable interfaces.
+#![cfg_attr(target_os = "none", no_std)]
+
+pub mod font_map;
+pub mod glyph;
